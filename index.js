8d13fea4318b876c04016cec8d30c9eec215a478
Wolf <58687191+WolfPilgrim@users.noreply.github.com>
2020-01-05 17:45:27 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
-    makesound(buttonInnerHTML);
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keydown", function(event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -49,7 +51,15 @@ function makeSound(key) {
       break;
 
     default:
-      console.log("buttonInnerHTML");
+      console.log("key");
 
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
